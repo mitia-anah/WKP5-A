cbd7fc04902536af8721c97f9e8b1fa7ef27a4d7
Rosny Ratsianompo <rosny.rat@onja.org>
2020-07-31 12:02:26 +0100

    start teh wkp5

--- script.js
+++ script.js
@@ -50,9 +50,52 @@ const recipes = [
 
 const renderCard = () => {
 	// check the recipes collection
+
 	// generate the HTML
-	// put it in the DOM
-};
+	const container = document.querySelector('.container');
+
+	recipes.forEach(element => {
+		const recipesHTML = `
+			<div class="card">
+				<h2>${element.title}</h2>
+				<img src="${element.picture}" alt="">
+				<p data-id="">
+				<ul>
+				   <li>Timing</li>
+				   <li>Difficulty</li>
+				</ul>
+				<button classe="btn">More Info</button>
+			</div>
+			`;
+		container.innerHTML = recipesHTML;
+	    });
+	};
+	
+	// Creat modal 
+	const outerModal = document.querySelector('.outer-modal');
+	const innerModal = document.querySelector('.inner-modal');
+	const moreInfoBtn = document.querySelector('.btn');
+
+
+	const showMoreInfo = ingredients => {
+		// Grab the detail
+		
+		if (e.target.matches('btn')) {
+			const parent = e.target.closest('.card');
+			const id = number(parent.dataset.id);
+		}
+		
+		// Creat the innerHtml
+		
+		// open the modal 
+		
+	
+	
+	// Function to close the modal
+	}
+
+
+// put it in the DOM
 
 const generateButton = document.querySelector('button.generate');
 generateButton.addEventListener('click', renderCard);
